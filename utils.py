a462256d9029122b90543b94c0714a40cf96638b
Jihwan <cuchoco@naver.com>
2021-12-21 02:27:38 +0000

    commit

---
+++ utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def pprint(msg, A):
+    print("---", msg, "---")
+    (n, m) = A.shape
+    for i in range(0,n):
+        line = ""
+        for j in range(0, m):
+            line += "{0:.2f}".format(A[i,j]) + "\t"
+        print(line)
+    print("")
+
+
+def npr(array):
+    return np.array(array)
+
+
+print(__name__)
